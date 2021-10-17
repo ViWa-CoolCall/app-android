c184dd059c5596c1f52a6c4845f4a34dd3ca2b87
Daniel Marques <daniel.brz2009@gmail.com>
2021-10-16 21:44:53 -0300

    first commit

---
+++ app/src/main/java/br/com/coolcall/HomeFragment.kt
@@ -0,0 +1,31 @@
+package br.com.coolcall
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import br.com.coolcall.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnHome.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToPortaisFragment()
+            view.findNavController().navigate(action)
+        }
+    }
+}
